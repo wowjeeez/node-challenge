660855e7391157427ce7d91660f84e1ccc977b02
Léderer András <andrew@file.glass>
2022-04-04 23:31:42 +0200

    chore(users/tests): write test for user creation

--- packages/domains/user/tests/query.test.ts
+++ packages/domains/user/tests/query.test.ts
@@ -10,4 +10,9 @@ describe('[Packages | User-domain | E2E]', () => {
     const res = await Api.get('/user/v1/get-user-details?userId=da140a29-ae80-4f0e-a62d-6c2d2bc8a475').expect(500);
     expect(res.body.status).toEqual(404);
   });
+
+  test('should create a new user', async () => {
+    const res = await Api.post('/user/v1/create-user').send({ firstName: 'Test', lastName: 'Agent 2', companyName: 'Fleo' }).expect(200);
+    expect(res.body).toEqual({ created: true });
+  });
 });
